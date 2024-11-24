9bcf8266055527b78d2dec25f9de8fa30a02edfa
suraj <surajankola9@gmail.com>
2024-11-24 19:14:10 +0530

    about section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./component/About";
 import Hero from "./component/Hero";
 
 const App = () => {
@@ -5,7 +6,7 @@ const App = () => {
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
 
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About />
     </main>
   );
 };
